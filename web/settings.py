41919f76d6ef72d5fae78e3ed501926d924fc925
Leonard Fabris <leonardfabris3@gmail.com>
2021-12-12 17:38:43 +0100

    domena

--- web/settings.py
+++ web/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8@fz4&n3)gz*1yk6tecf80dp4tsi)2zh5ehx3#nteeechx%o=p'
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'cleannature.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'foodcopter.herokuapp.com']
 
 
 INSTALLED_APPS = [
